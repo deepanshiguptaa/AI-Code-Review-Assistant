c614a80fb2ff13831569adabb3f413d0f063d1c2
Deepanshi <deepanshigupta58@gmail.com>
2026-04-07 17:31:44 +0530

    connected atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ai-code-review");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 };
+
+export default connectDB;
