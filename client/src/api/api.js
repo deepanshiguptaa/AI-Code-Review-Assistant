711e022abf368c47e1d7cfb8791375442b021adb
Deepanshi <deepanshigupta58@gmail.com>
2026-04-27 14:56:55 +0530

    render url added

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://ai-code-review-assistant-7xl9.onrender.com",
 });
 
 export default api;
